e78119da2ecbc4e72df8c7a5f122c09de9e44c2e
SytHamMer <mathys.lebon1@gmail.com>
2023-01-05 16:52:51 +0100

    Features : Getters on Questions

--- src/model/QuestionMaker.py
+++ src/model/QuestionMaker.py
@@ -1,5 +1,16 @@
 class Question:
+
     def __init__(self,question,propositions,goodAnswer):
         self.question = question
         self.propositions = propositions
         self.goodAnswer = goodAnswer
+
+
+    def getQuestion(self):
+        return self.question
+    
+    def getGoodAnswer(self):
+        return self.goodAnswer
+
+    def getPropositions(self):
+        return self.propositions
