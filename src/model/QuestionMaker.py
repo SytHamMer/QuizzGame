907d17d7ed44832ff58306ea7118e03fa444b957
SytHamMer <mathys.lebon1@gmail.com>
2023-01-05 11:36:47 +0100

    Features : Add two fonctions to handle answer validity

---
+++ src/model/QuestionMaker.py
@@ -0,0 +1,5 @@
+class Question:
+    def __init__(self,question,propositions,goodAnswer):
+        self.question = question
+        self.propositions = propositions
+        self.goodAnswer = goodAnswer
