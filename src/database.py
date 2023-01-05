c2d9c6dcdf401ec4afc07eb2de48fe11a6922227
Ugo Tafaro <utafaro@gmail.com>
2023-01-05 11:30:28 +0100

    base de données

---
+++ src/database.py
@@ -0,0 +1,80 @@
+import sqlite3
+
+conn = sqlite3.connect('ma_base.db')
+
+cursor = conn.cursor()
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Score(
+
+    idScore INTEGER PRIMARY KEY UNIQUE,
+    date DATE
+
+
+
+)
+
+""")
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Utilisateur(
+
+    pseudo TEXT PRIMARY KEY UNIQUE,
+    mdp VARCHAR(250),
+    estAdmin BOOLEAN,
+    idScore INT,
+    FOREIGN KEY(idScore) REFERENCES Score(idScore)
+
+
+
+)
+
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Questions(
+    idQuest INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    question TEXT,
+    proposition VARCHAR(250),
+    bonne_reponse TEXT
+  
+
+)
+
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Quizz(
+    idQuizz INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+    nom TEXT,
+    type TEXT,
+    image VARCHAR(250),
+    idQuest INT,
+    FOREIGN KEY(idQuest) REFERENCES Questions(idQuest)
+
+)
+
+""")
+
+
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS joue(
+    iduti TEXT,
+    idQuizz INT,
+    nbpoints INT,
+    PRIMARY KEY(iduti, idQuizz)
+    FOREIGN KEY(iduti) REFERENCES Utilisateur(pseudo)
+    FOREIGN KEY(idQuizz) REFERENCES Score(idQuizz)
+)
+""")
+
+
+
+
+
+conn.commit()
+
+
