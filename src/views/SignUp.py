3554be8601681cfb7cdbe35148ea9970954ca74f
Arnaud Pfundstein <arnaudpfundstein.pro@gmail.com>
2023-01-10 11:17:31 +0100

    Clean: create the createTables function and change the db model of the tables.

--- src/views/SignUp.py
+++ src/views/SignUp.py
@@ -10,30 +10,33 @@ class SignUp:
         self.app.setCurrentFrame('game')
 
     def __render(self):
-        back = Frame(self.document,bg= "#7FB8ED", borderwidth=0,height=450,width=855)
+        back = Frame(self.document, bg="#7FB8ED",
+                     borderwidth=0, height=450, width=855)
         back.pack(expand=YES)
 
-        userLbl = Label(back, text='Username', bg='#7FB8ED', fg='#31468F', borderwidth=0, font=('Inter', 28))
+        userLbl = Label(back, text='Username', bg='#7FB8ED',
+                        fg='#31468F', borderwidth=0, font=('Inter', 28))
         userLbl.place(x=60, y=52)
 
-        userEntry=Entry(back, width=40, font=('arial',20))
+        userEntry = Entry(back, width=40, font=('arial', 20))
         userEntry.place(x=240, y=52)
 
-        pwLbl = Label(back, text='Password', bg='#7FB8ED', fg='#31468F', borderwidth=0, font=('Inter', 28))
+        pwLbl = Label(back, text='Password', bg='#7FB8ED',
+                      fg='#31468F', borderwidth=0, font=('Inter', 28))
         pwLbl.place(x=60, y=152)
-        
-        pwEntry= Entry(back, width=40, font=('arial',20))
+
+        pwEntry = Entry(back, width=40, font=('arial', 20))
         pwEntry.place(x=240, y=152)
 
-        
-
-        cpwLbl = Label(back, text='Confirm\n Password', bg='#7FB8ED', fg='#31468F', borderwidth=0, font=('Inter', 28))
+        cpwLbl = Label(back, text='Confirm\n Password', bg='#7FB8ED',
+                       fg='#31468F', borderwidth=0, font=('Inter', 28))
         cpwLbl.place(x=60, y=252)
 
-        cpwEntry = Entry(back, width=40, font=('arial',20))
+        cpwEntry = Entry(back, width=40, font=('arial', 20))
         cpwEntry.place(x=240, y=262)
 
-        SignUpButton = Button(back, text='Sign Up',bg='#31468F', fg='white', activebackground='#052B71',  font=('Inter', 40), command=self.handleClick)
+        SignUpButton = Button(back, text='Sign Up', bg='#31468F', fg='white',
+                              activebackground='#052B71',  font=('Inter', 40), command=self.handleClick)
         SignUpButton.place(x=350, y=320)
 
         pass
